265e42857cfd5ca426a16ef463ccae64ca0b9b66
sk1pY <sk1py@bk.ru>
2025-06-26 02:59:01 +0300

    port

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ sed -i '/^MAIL_FROM_NAME=/c\MAIL_FROM_NAME="Demo App"' .env
 echo "4 Настройка БД"
 sed -i '/^DB_CONNECTION=/c\DB_CONNECTION=mysql' .env
 sed -i '/^DB_HOST=/c\DB_HOST=mysql' .env
-sed -i '/^DB_PORT=/c\DB_PORT=3307' .env
+sed -i '/^DB_PORT=/c\DB_PORT=3306' .env
 sed -i '/^DB_DATABASE=/c\DB_DATABASE=bookshop' .env
 sed -i '/^DB_USERNAME=/c\DB_USERNAME=root' .env
 sed -i '/^DB_PASSWORD=/c\DB_PASSWORD=' .env
